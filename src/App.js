192ba617e2f6e1efd678010f142f7d9029849489
Danielle Croft <daniellec0321@gmail.com>
2024-03-05 17:36:56 -0500

    It's bad!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import profilePicture from './images/profile-picture.heic';
 import './App.css';
 
@@ -7,17 +6,18 @@ function App() {
     <div className="app-background">
       <header className="App-header">
         <img src={profilePicture} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h1>
+          Danielle Croft
+        </h1>
+        <center><a className='github-logo' href="https://github.com/daniellec0321" target="_blank"></a></center>
+        {/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
       </header>
     </div>
   );
