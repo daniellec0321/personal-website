e86161546bfc6b83ac1b7cfb9dae805064a33661
Danielle Croft <daniellec0321@gmail.com>
2024-03-06 13:20:09 -0500

    Added routing to pages

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Main from "./Main/Main.js";
+import Education from "./Education/Education.js";
+import Projects from "./Projects/Projects.js";
+import Resume from "./Resume/Resume.js";
 import "../App.css"
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 /* Routing for our app. */
 function Components() {
@@ -9,6 +12,9 @@ function Components() {
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
         </Routes>
       </Router>
     );
---
+++ src/Components/Education/Education.js
@@ -0,0 +1,9 @@
+import '../../App.css';
+
+function Education() {
+  return (
+    <h1>education</h1>
+  );
+}
+
+export default Education; 
