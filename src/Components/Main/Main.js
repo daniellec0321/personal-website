cf5836d4cdde553e2fbb852556c18f1fbe413635
Danielle Croft <daniellec0321@gmail.com>
2024-05-20 16:58:13 -0400

    CSS for main page is epic

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "../Header/Header";
+import { Link } from "react-router-dom";
 
 function Main() {
   return (
     <>
     <Header />
     <div className="page">
+      <div>
         <h2>My name is Danielle Croft, and this is my personal website!</h2>
-        <p>I am a recent graduate from the University of Notre Dame with a major in Computer Science. On this website, I'll share my education, employment, and coding projects. rahhhhh</p>
+        <h4>I am a recent graduate from the University of Notre Dame with a major in Computer Science. On this website, I'll share about myself, my education, and my coding projects.</h4>
+      </div>
+      <div className="mainPageArticleContainer">
+        <Link to="/about" className="aboutLink">About</Link>
+        <Link to="/education" className="educationLink">Education</Link>
+        <Link to="/projects" className="projectsLink">Projects</Link>
+      </div>
     </div>
     </>
   );
