1e760356b6fc021d03a3ed3f28d7af432e52ac7f
Danielle Croft <daniellec0321@gmail.com>
2024-06-06 12:53:36 -0400

    Trying to download resume

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import fileSaver from 'file-saver';
+
+const saveFile = () => {
+  fileSaver.saveAs(
+    "../Header/Header.js",
+    "resume.js"
+  );
+}
+// <button className="resume" onClick={saveFile}>Download Resume</button>
 
 function Experience() {
   return (
     <>
     <Header />
     <div className="page">
-      <h1>Experience Page</h1>
+      <h2>My Experience</h2>
+      <div className="resume-content">
+        <a href="../../resources/resume.pdf">Download CV</a>
+      </div>
+      <h4>Below is a detailed list of the jobs I have held.</h4>
     </div>
     <Footer />
     </>
