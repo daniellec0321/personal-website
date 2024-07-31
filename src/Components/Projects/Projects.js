9a68f6808cec609a64062d2765b01cfc38ffbf89
Danielle Croft <daniellec0321@gmail.com>
2024-07-31 11:49:04 -0500

    Began projects page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { HashLink } from 'react-router-hash-link';
 
 function Projects() {
   return (
@@ -8,7 +9,7 @@ function Projects() {
     <Header />
     <div className="page">
       <h2>My Projects & Patents</h2>
-      <h4>This page is currently a work in progress. Come back soon to see my projects and patents! :D</h4>
+      <HashLink smooth to='/projects/#test' className="project-button"> History </HashLink>
     </div>
     <Footer />
     </>
