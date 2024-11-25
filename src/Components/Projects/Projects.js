c6f3f19d4a6d61921fbc9de982a2b404a4a746b3
Danielle Croft <daniellec0321@gmail.com>
2024-11-24 21:10:54 -0500

    Installed necessary packages for projects page and updated the CSS

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { HashLink } from 'react-router-hash-link';
 
 function Projects() {
   return (
@@ -9,6 +10,8 @@ function Projects() {
     <div className="page">
       <h2>My Projects & Patents</h2>
       <h4>This page is currently a work in progress. Come back soon to see my projects and patents! :D</h4>
+      <HashLink smooth to='/projects#test' className="project-button">This button should go to the hashlink.</HashLink>
+      <h6 id="test">Here is the hashlink.</h6>
     </div>
     <Footer />
     </>
