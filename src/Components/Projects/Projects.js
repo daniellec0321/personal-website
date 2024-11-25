38e5239ef1279d40a3dbfac9447a4f8c5256f217
Danielle Croft <daniellec0321@gmail.com>
2024-11-24 21:20:13 -0500

    Began creating the buttons for the projects page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { HashLink } from 'react-router-hash-link';
+import { Link } from "react-router-dom";
 
 function Projects() {
   return (
@@ -10,8 +11,9 @@ function Projects() {
     <div className="page">
       <h2>My Projects & Patents</h2>
       <h4>This page is currently a work in progress. Come back soon to see my projects and patents! :D</h4>
-      <HashLink smooth to='/projects#test' className="project-button">This button should go to the hashlink.</HashLink>
-      <h6 id="test">Here is the hashlink.</h6>
+      <div className="mainPageArticleContainer">
+        <HashLink smooth to="/projects#patent" className="projects-patentLink">Patent</HashLink>
+      </div>
     </div>
     <Footer />
     </>
